b147cdd314d5500dff37ba11d1246bef6592e44e
nieanqin <1667639114@qq.com>
2024-06-21 18:14:52 +0800

    Of the project5.0
    Realize redirection

--- execute.cpp
+++ execute.cpp
@@ -1,5 +1,7 @@
 #include "smsh.h"
 #include <sys/wait.h>
+#include <sstream>
+#include <fcntl.h>
 
 void execute2(char *arglist[]){
     execvp(arglist[0],arglist);
@@ -31,6 +33,22 @@ void execute3(char *arglist[]){
 int execute4(char *arglist[]){
     int pid;
     int child_info = -1;
+    std::vector<std::string> args;
+    std::string infile, outfile;
+
+    size_t size;
+    for (size = 0; arglist[size] != NULL; ++size) {
+    }
+    for (size_t j = 0; j <size; ++j) {
+
+        if (*arglist[j] == '<' && j + 1 < size) {
+            infile = arglist[++j];
+        } else if (*arglist[j] == '>' && j + 1 < size) {
+            outfile = arglist[++j];
+        } else {
+            args.push_back(arglist[j]);
+        }
+    }
 
     if(arglist[0]==NULL)
         return 0;
@@ -39,7 +57,23 @@ int execute4(char *arglist[]){
     else if (pid == 0) {
         signal(SIGINT,SIG_DFL);
         signal(SIGQUIT,SIG_DFL);
-        execvp(arglist[0],arglist);
+        if (!infile.empty()) {
+            int in_fd = open(infile.c_str(), O_RDONLY);
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
+        }
+        if (!outfile.empty()) {
+            int out_fd = open(outfile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
+        }
+        std::vector<char*> c_args;
+        for (auto& arg : args) {
+            c_args.push_back(&arg[0]);
+        }
+        c_args.push_back(nullptr);
+        execvp(c_args[0], c_args.data());
+//        execvp(arglist[0],arglist);
         perror("execvp failed");
         exit(1);
     }
